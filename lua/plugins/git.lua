924e004ad3d093ddc6e5a0ed557ec41837f7105b
Timothy <1695613+timothyis@users.noreply.github.com>
2024-06-01 14:24:01 +0100

    Add gitsigns

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,23 @@
 return {
-    'tpope/vim-fugitive',
-    keys = {
-        { "<leader>gs", "<cmd>Git<cr>", desc = "Git Status" }
-    }
+    -- Git UI
+    {
+        'tpope/vim-fugitive',
+        keys = {
+            { "<leader>gs", "<cmd>Git<cr>", desc = "Git Status" }
+        },
+    },
+
+    -- Git signs in the gutter
+    {
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
 }
