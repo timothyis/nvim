18fdf9e92adeda89f3368d1ac8ca6fe0e368abdf
Timothy <1695613+timothyis@users.noreply.github.com>
2024-08-01 23:03:09 +0100

    Update which-key to use new spec config

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -4,31 +4,27 @@ return {
         "folke/which-key.nvim",
         event = "VimEnter",
         opts = {
-            plugins = { spelling = true },
+            spec = {
+                {
+                    mode = { "n", "v" },
+                    { "<leader>b", group = "buffer" },
+                    { "<leader>c", group = "code" },
+                    { "<leader>f", group = "file/find" },
+                    { "<leader>g", group = "git" },
+                    { "<leader>gh", group = "hunks" },
+                    { "<leader>h", group = "harpoon" },
+                    { "<leader>pv", group = "projectfiles" },
+                    { "<leader>q", group = "quit/session" },
+                    { "<leader>s", group = "search" },
+                    { "<leader>u", group = "ui" },
+                    { "<leader>w", group = "windows" },
+                    { "<leader>x", group = "diagnostics/quickfix" },
+                    { "[", group = "prev" },
+                    { "]", group = "next" },
+                    { "g", group = "goto" },
+                    { "gz", group = "surround" },
+                },
+            },
         },
-        config = function(_, opts)
-            local wk = require("which-key")
-            wk.setup(opts)
-            local keymaps = {
-                mode = { "n", "v" },
-                ["g"] = { name = "+goto" },
-                ["gz"] = { name = "+surround" },
-                ["]"] = { name = "+next" },
-                ["["] = { name = "+prev" },
-                ["<leader>pv"] = { name = "+projectfiles" },
-                ["<leader>b"] = { name = "+buffer" },
-                ["<leader>c"] = { name = "+code" },
-                ["<leader>f"] = { name = "+file/find" },
-                ["<leader>g"] = { name = "+git" },
-                ["<leader>gh"] = { name = "+hunks" },
-                ["<leader>h"] = { name = "+harpoon" },
-                ["<leader>q"] = { name = "+quit/session" },
-                ["<leader>s"] = { name = "+search" },
-                ["<leader>u"] = { name = "+ui" },
-                ["<leader>w"] = { name = "+windows" },
-                ["<leader>x"] = { name = "+diagnostics/quickfix" },
-            }
-            wk.register(keymaps)
-        end,
     },
 }
